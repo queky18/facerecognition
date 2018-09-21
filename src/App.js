d441fd3050672f27078a4326b431855212b3edc9
Adrian Gheorghe <adrian.gh.m.18@gmail.com>
2018-09-22 02:13:38 +0300

    particle UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import Particles extendss Components;
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from "./components/ImageLinkFrom/ImageLinkFrom";
 import Rank from "./components/Rank/Rank";
 import './App.css';
 
+const particlesOptions = {
+    particles : {
+      line_linked : {
+        shadow : {
+          enable : true,
+          color : '#3CA9D1',
+        }
+      }
+    }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles params={particlesOptions}
+        params = "1500";
+            />
         <Navigation />
         <Logo />
         <Rank />
