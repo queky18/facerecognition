5989e9482e94dda4f25fbb666c8979656c4408c9
Adrian Gheorghe <adrian.gh.m.18@gmail.com>
2018-09-22 02:06:26 +0300

    React Rank

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Rank.css';
+
+const Rank = () => {
+    return (
+        <div>
+            <div className='white f3'>
+                {'Adrian, your current rank is ....'}
+            </div>
+            <div className ='white f1'>
+                {'#1'}
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
