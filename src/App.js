8dfd141cf5b4fe987b1d15b4cd4c565a03f1a5a6
Adrian Gheorghe <adrian.gh.m.18@gmail.com>
2018-09-22 02:05:54 +0300

    up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from "./components/ImageLinkFrom/ImageLinkFrom";
+import Rank from "./components/Rank/Rank";
 import './App.css';
 
 class App extends Component {
@@ -9,8 +11,9 @@ class App extends Component {
       <div className="App">
         <Navigation />
         <Logo />
-        {/* <ImageLinkForm />
-        <FaceRecognition /> */}
+        <Rank />
+        <ImageLinkForm />
+        {/* <FaceRecognition /> */}
       </div>
     );
   }
