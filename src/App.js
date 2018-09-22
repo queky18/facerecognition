670b995dd115e2b24b09cd35b4dcea68cbee4639
Adrian Gheorghe <adrian.gh.m.18@gmail.com>
2018-09-22 13:06:01 +0300

    Particle Js Config

--- src/App.js
+++ src/App.js
@@ -1,29 +1,146 @@
 import React, { Component } from 'react';
-import Particles extendss Components;
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from "./components/ImageLinkFrom/ImageLinkFrom";
 import Rank from "./components/Rank/Rank";
 import './App.css';
 
+/* 
+Heads up that the clarifai API has been updated since I made the next video.
+You will get an error using Clarifai.DETECT_FACE,  it appears to have changed to 
+Clarifai.FACE_DETECT_MODEL
+
+app.models
+.predict(
+Clarifai.COLOR_MODEL,
+    // URL
+    "https://samples.clarifai.com/metro-north.jpg"
+)
+.then(function(response) {
+    // do something with responseconsole.log(response);
+    },
+    function(err) {// there was an error}
+);
+
+*/
+
 const particlesOptions = {
-    particles : {
-      number : {
-        value : 30,
-        density :{
-          enable : true,
-          value_area : 800
+    particles: {
+      number: {
+        value: 80,
+        density: {
+          enable: true,
+          value_area: 800
+        }
+      },
+      color: {
+        value: "#ffffff"
+      },
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0,
+          color: "#000000"
+        },
+        polygon: {
+          nb_sides: 5
+        },
+        image: {
+          src: "img/github.svg",
+          width: 100,
+          height: 100
+        }
+      },
+      opacity: {
+        value: 0.5,
+        random: false,
+        anim: {
+          enable: false,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false
+        }
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 40,
+          size_min: 0.1,
+          sync: false
+        }
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1
+      },
+      move: {
+        enable: true,
+        speed: 6,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200
         }
       }
-    }
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "grab"
+        },
+        onclick: {
+          enable: true,
+          mode: "push"
+        },
+        resize: true
+      },
+      modes: {
+        grab: {
+          distance: 400,
+          line_linked: {
+            opacity: 1
+          }
+        },
+        bubble: {
+          distance: 400,
+          size: 40,
+          duration: 2,
+          opacity: 8,
+          speed: 3
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4
+        },
+        push: {
+          particles_nb: 4
+        },
+        remove: {
+          particles_nb: 2
+        }
+      }
+    },
+    retina_detect: true
 }
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Particles params={particlesOptions}
-        params = "1500";
+        <Particles className='particles' params={particlesOptions}
             />
         <Navigation />
         <Logo />
