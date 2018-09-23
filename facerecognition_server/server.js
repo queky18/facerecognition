bef238922de96a9a242f0a41badea68c144fccc4
Adrian Gheorghe <adrian.gh.m.18@gmail.com>
2018-09-23 15:45:51 +0300

    aa

--- facerecognition_server/server.js
+++ facerecognition_server/server.js
@@ -13,8 +13,8 @@ const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
+    user : 'adrian',
+    password : '1234',
     database : 'smart-brain'
   }
 });
